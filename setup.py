aa66cd373a9029be0ddfc9895e52d5580a1119f8
Za08 <95156875+Za08@users.noreply.github.com>
2022-04-28 15:57:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='AshEvent-Za08', version='0.2', author='Za08',
+    name='ashevent', version='0.2', author='Za08',
     description='A simple event system in Python',
     long_description=long_description, long_description_content_type='text/markdown',
     url='https://github.com/The-Ash-Team/AshEvent', license='MIT',
