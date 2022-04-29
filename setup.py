6e142674386c1a88b026a7247bbd423a3d7984f6
Za08 <95156875+Za08@users.noreply.github.com>
2022-04-29 14:21:41 +0800

    0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
+from re import finditer
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open(file="README.md", mode="r", encoding="utf-8") as f:
     long_description = f.read()
+with open(file="ashevent/__init__.py", mode="r", encoding="utf-8") as f:
+    version = next(finditer(r"__version__[ ]*=[ ]*[\'\"](.*?)[\"\']", f.read())).group(1)
 
 setup(
-    name='ashevent', version='0.2', author='Za08',
-    description='A simple event system in Python',
-    long_description=long_description, long_description_content_type='text/markdown',
-    url='https://github.com/The-Ash-Team/AshEvent', license='MIT',
+    name="ashevent", version=version, author="Za08",
+    description="A simple event system in Python",
+    long_description=long_description, long_description_content_type="text/markdown",
+    url="https://github.com/The-Ash-Team/AshEvent", license="MIT",
     packages=find_packages()
 )
